613fa38bcc050c0f235b78b3996a03f8eea0918f
emamoldo <moldovanemanuel92.em@gmail.com>
2024-04-12 17:40:40 +0200

    Add the structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+<body>
+    <h1>Bad Words</h1>
+
+    <form action="script.php" method="GET">
+        <input type="text" placeholder="Add the word" name="word" >
+        <button type="submit">Sand</button>
+    </form>
+
+</body>
+</html>
